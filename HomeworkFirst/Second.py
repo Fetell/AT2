022e4cd8d6fb0361f9b57af06a11df8f1f498b37
Fetell <fetell.eee@gmail.com>
2023-02-08 17:23:38 +0300

    Module 6 lesson 1 homework format

--- HomeworkFirst/Second.py
+++ HomeworkFirst/Second.py
@@ -1,3 +1,6 @@
 my_dict = {'a': 500, 'b': 5874, 'c': 560, 'd': 400, 'e': 5874, 'f': 20}
+
+print("Ключи с наибольшим значением:", end=" ")
+
 for key, value in sorted(my_dict.items(), key=lambda x: x[1], reverse=True)[:3]:
     print(key, "", end="")
--- HomeworkFirst/Third.py
+++ HomeworkFirst/Third.py
@@ -1,7 +1,10 @@
 import random
-rNum = random.randrange(100000)
-rSum = 0
-for i in str(rNum):
-    rSum = rSum + int(i)
-print("Случайное число: ", rNum)
-print("Сумма его цифр: ", rSum)
+
+random_number = random.randrange(100000)
+random_sum = 0
+
+for i in str(random_number):
+    random_sum = random_sum + int(i)
+
+print("Случайное число:", random_number)
+print("Сумма его цифр:", random_sum)
